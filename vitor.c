a69baed5a25249c512e74efd7e6d0d8e8f44e7a2
Vitorcarlos007 <vitorlimpo15@gmail.com>
2025-03-27 19:07:22 -0300

    primeiro commit

---
+++ vitor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 25;
+    float altura = 1.75;
+    char opcao = "S";
+    char nome[20] = "sergio";
+
+    printf("A idade do %s é %d\n", nome, idade);
+    printf("A altura é %e\n", altura);
+    printf("A opcao é :%c\n", opcao);
+}
